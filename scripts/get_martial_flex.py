eed59e694c58c9c140d222dca8c0c461be2dd2b0
Tom <tomoinn@gmail.com>
2019-08-02 14:49:14 +0100

    Added (t) to teamwork feats when showing martial flex options

--- scripts/get_martial_flex.py
+++ scripts/get_martial_flex.py
@@ -2,7 +2,9 @@ from pyfeats import MartialFlex, read_feat_csv
 
 m = MartialFlex(feats=read_feat_csv(),
                 known_feat_names=['combat expertise', 'outflank', 'following step',
-                                  'step up', 'armor proficiency', 'weapon proficiency', 'power attack'],
+                                  'step up', 'armor proficiency', 'weapon proficiency', 'power attack',
+                                  'simple weapon proficiency', 'martial weapon proficiency', 'shield proficiency',
+                                  'weapon proficiency'],
                 bab=5, fighter_level=5, monk_level=0, brawler_level=0,
                 str_stat=20, con_stat=12, dex_stat=16, wis_stat=9, int_stat=14, cha_stat=7, race='human', deity='gorum')
 
